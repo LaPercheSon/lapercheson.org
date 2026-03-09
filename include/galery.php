31d7261f089756f9553453b26c12fc1c26220b30
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2026-03-09 22:09:31 +0100

    fix: galery + refactor statuts

--- include/galery.php
+++ include/galery.php
@@ -20,6 +20,51 @@ $_TOURNAGE = [
     [
         getStrList("galery/teaserInterception",4),
         "Teaser Interception",
+        [["YouTube", "https://www.youtube.com/watch?v=LD6vmeCJGw4"]]
+    ],
+    [
+        getStrList("galery/intercepStairs",2),
+        "Parodie de l'escalier d'Escher d'Inception",
+        []
+    ],
+    [
+        getStrList("galery/cestPasSorcier",3),
+        "Parodie C'est Pas Sorcier",
+        []
+    ],
+    [
+        getStrList("galery/plane",2),
+        "EN20 ANS - Scène de l'avion",
+        []
+    ],
+    [
+        ["galery/pilotageDrone.jpg"],
+        "Plans aériens",
+        []
+    ],
+    [
+        getStrList("galery/spirituCucumis",4),
+        "1er 48HFP : Spiritu Cucumis",
+        [["YouTube", "https://www.youtube.com/watch?v=ywBxVbaI0AM"]]
+    ],
+    [
+        getStrList("galery/parodieEmissions",2),
+        "Parodies d'émission télé",
+        []
+    ],
+    [
+        ["galery/parodieYoutube.jpg"],
+        "Scène bureau",
+        []
+    ],
+    [
+        getStrList("galery/enquetedenquetes",2),
+        "Parodie En Quête d'enquêtes",
+        []
+    ],
+    [
+        getStrList("galery/captation",2),
+        "Captation d'événement",
         []
     ],
 ];
@@ -47,8 +92,8 @@ function photocrea($pre, $batch, $crea): void
 ?>
     <figure class="minigallery">
         <?php
-        foreach ($crea[0] as $merde => $image) {
-            $id = 'img_' . $pre . '_s' . $batch . '_' . $merde;
+        foreach ($crea[0] as $idx => $image) {
+            $id = 'img_' . $pre . '_s' . $batch . '_' . $idx;
             $src = "images/" . htmlspecialchars($image, ENT_QUOTES);
             echo "<label class='thumb' for='$id' tabindex='0'>";
             echo "<img loading='lazy' class=\"cImg\" src='" . $src . "' alt=''>";
@@ -72,8 +117,8 @@ function photocrea($pre, $batch, $crea): void
 
     <div class="creationDescription">
         <p><?php
-            $crea[1];
-            foreach ($crea[2] as $key => $link) {
+            echo $crea[1];
+            foreach ($crea[2] as $_key => $link) {
                 echo " <a target=\"_blank\" href=\"" . htmlspecialchars($link[1], ENT_QUOTES) . "\" class=\"clink\">&lt;" . $link[0] . "&gt;</a>";
             }
             ?></p>
