d4ce52c2278eb6e2d1e32da0ed081f63715b72a3
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2026-03-15 18:26:19 +0100

    feat: contact section

--- include/sections/contact.php
+++ include/sections/contact.php
@@ -1,23 +1,35 @@
 <?php newSection(); ?>
+<p class="main_section_margin">Pour plus d'informations sur <strong>La Perche Son</strong> : </p>
+<ul class="blockPage film-strip" id="listContact">
 
-<ul class="blockPage" id="listContact">
+    <li class="frame-strip empty-frame"></li>
 
-    <li id="aboutContact">
-        <h4>À Propos</h4>
-        <a href="#aboutPopUp"><img src="images/icons/about.svg" class="invertOk"></a>
-        <hr>
-        <p>
-            Qui sommes-nous ?<br>Plus d'informations sur l'association et ce site web.
-        </p>
-    </li>
+    <div class="frame-separator"></div>
 
-    <li id="mailContact">
-        <h4>Envoyer un mail</h4>
-        <a href="mailto:contact@lapercheson.org"><img src="images/icons/mail.svg" class="invertOk"></a>
-        <hr>
-        <p>
-            Pour toute question n'hésitez pas à envoyer un mail.
-        </p>
-    </li>
+    <a href="#aboutPopUp">
+        <li id="aboutContact" class="frame-strip full-frame">
+            <div class="frame-content">
+                <h4>À Propos</h4>
+                <p>Qui sommes-nous ?<br>Plus d'informations sur l'association et ce site web.</p>
+            </div>
+        </li>
+    </a>
+
+    <div class="frame-separator"></div>
+
+    <a href="mailto:contact@lapercheson.org">
+        <li id="mailContact" class="frame-strip full-frame">
+            <div class="frame-content">
+                <h4>Envoyer un mail</h4>
+                <p>Pour toute question n'hésitez pas à envoyer un mail.</p>
+            </div>
+        </li>
+    </a>
+
+    <div class="frame-separator"></div>
+
+    <li class="frame-strip empty-frame"></li>
 
 </ul>
+
+<?php printVHeight(); ?>
