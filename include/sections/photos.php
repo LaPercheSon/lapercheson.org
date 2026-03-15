af61ef08c5f593df5ad0a53d0d48fa24623f1bce
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2026-03-15 19:19:18 +0100

    feat: main button link

--- include/sections/photos.php
+++ include/sections/photos.php
@@ -2,4 +2,4 @@
 newSection();
 creas2html($_TOURNAGE, "tournage", "IMG");
 ?>
-<a href="./photos">Voir plus de photos</a>
+<a href="./photos" class="central_main_button">Voir plus de photos <span class="open_link_indicator">&gt;</span></a>
