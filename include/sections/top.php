49418973143c902fe74f1c20f0efaa59fce25ae8
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2026-03-17 00:06:24 +0100

    fix: project style on phones

--- include/sections/top.php
+++ include/sections/top.php
@@ -5,3 +5,4 @@ printVHeight();
 
 <h1 class="ovline unline unov_anim unov_relative">La Perche Son</h1>
 <sub class="main_sub">Collectif de création audiovisuelle</sub>
+<img src="images/icons/logo.png" id="main_logo">
